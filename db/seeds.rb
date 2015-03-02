d1d9fbfe9c4cbd93ac5cb9952740a2c07bfc6ff6
cdelmoral <carderomad@gmail.com>
2015-03-01 23:21:16 -0600

    10.1 add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name: "Example User",
-             email: "example@railstutoria.org",
+             email: "example@railstutorial.org",
              password: "foobar",
              password_confirmation: "foobar",
              admin: true,
